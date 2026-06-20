40fffe4c44d96a3b7df94b078f34c7fde5d2941c
DZbizzarre <jareddiazrodriguez@gmail.com>
2026-06-19 21:35:34 -0600

    pryecto
    aqui esta el codigo del login y asistencia

---
+++ aplicacion_asistencia/src/aplicacion_asistencia/Aplicacion_asistencia.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplicacion_asistencia;
+
+import aplicacion_asistencia.vista.login;
+
+/**
+ *
+ * @author Jared
+ */
+public class Aplicacion_asistencia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        java.awt.EventQueue.invokeLater(() -> new login().setVisible(true));
+    }
+    
+}
