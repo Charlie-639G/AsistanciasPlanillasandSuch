62e8be084fc7f65ebe43d20986542d2c918355ef
Supinv <hannahgc292007@gmail.com>
2026-06-21 22:29:58 -0600

    Jframe admin
    aqui se esta haciendo la vista de los admins

---
+++ admin/src/main/java/com/mycompany/admin/Admin.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.admin;
+
+/**
+ *
+ * @author HP
+ */
+public class Admin {
+
+    public static void main(String[] args) {
+       sistema_admin menu = new sistema_admin();
+       menu.setTitle ("Sistema de Administracion");
+       menu.setLocationRelativeTo (null);
+       menu.setVisible(true);
+    }
+}
