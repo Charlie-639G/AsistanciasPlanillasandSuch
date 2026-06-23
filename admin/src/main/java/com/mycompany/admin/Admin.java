d7dbae71d44a6de3cc37d9bd850b2db4e82f8391
Supinv <hannahgc292007@gmail.com>
2026-06-23 17:38:50 -0600

    sistema que ve el admin
    se completo la parte q ve el admin y un poco de la programacion

---
+++ admin/src/main/java/com/mycompany/admin/Admin.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.admin;
+
+/**
+ *
+ * @author HP
+ */
+public class Admin {
+
+    public static void main(String[] args) {
+       Principal menu = new Principal();
+       menu.setTitle ("Sistema de Administración");
+       menu.setLocationRelativeTo (null);
+       menu.setVisible(true);
+    }
+}
